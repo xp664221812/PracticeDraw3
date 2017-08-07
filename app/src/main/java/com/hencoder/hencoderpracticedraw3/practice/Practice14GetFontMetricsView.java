c8d92d672a5325e5422f4ce5c4a5f0ea1cc7575d
xp664221812 <realxp141414@163.com>
2017-08-07 12:48:20 +0800

    finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
@@ -45,11 +45,14 @@ public class Practice14GetFontMetricsView extends View {
         // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(texts[0], 100, middle, paint2);
-        canvas.drawText(texts[1], 200, middle, paint2);
-        canvas.drawText(texts[2], 300, middle, paint2);
-        canvas.drawText(texts[3], 400, middle, paint2);
-        canvas.drawText(texts[4], 500, middle, paint2);
-        canvas.drawText(texts[5], 600, middle, paint2);
+
+        Paint.FontMetrics fontMetrics = paint2.getFontMetrics();
+
+        canvas.drawText(texts[0], 100, middle - (fontMetrics.top + fontMetrics.bottom) / 2, paint2);
+        canvas.drawText(texts[1], 200, middle - (fontMetrics.top + fontMetrics.bottom) / 2, paint2);
+        canvas.drawText(texts[2], 300, middle - (fontMetrics.top + fontMetrics.bottom) / 2, paint2);
+        canvas.drawText(texts[3], 400, middle - (fontMetrics.top + fontMetrics.bottom) / 2, paint2);
+        canvas.drawText(texts[4], 500, middle - (fontMetrics.top + fontMetrics.bottom) / 2, paint2);
+        canvas.drawText(texts[5], 600, middle - (fontMetrics.top + fontMetrics.bottom) / 2, paint2);
     }
 }
