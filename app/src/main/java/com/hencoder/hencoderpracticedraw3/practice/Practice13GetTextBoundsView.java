05cda125f3cdc8ce805e4d9c040bcc8d57bc2089
xp664221812 <realxp141414@163.com>
2017-08-07 08:57:09 +0800

    2dn

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -48,13 +49,20 @@ public class Practice13GetTextBoundsView extends View {
         // 使用 Paint.getTextBounds() 计算出文字的显示区域
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
-
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        Rect bonds=new Rect();
+
+        paint2.getTextBounds(text1,0,text1.length(),bonds);
+        canvas.drawText(text1, 100, middle-(bonds.top+bonds.bottom)/2, paint2);
+        paint2.getTextBounds(text2,0,text2.length(),bonds);
+        canvas.drawText(text2, 200,  middle-(bonds.top+bonds.bottom)/2, paint2);
+        paint2.getTextBounds(text3,0,text3.length(),bonds);
+        canvas.drawText(text3, 300,  middle-(bonds.top+bonds.bottom)/2, paint2);
+        paint2.getTextBounds(text4,0,text4.length(),bonds);
+        canvas.drawText(text4, 400,  middle-(bonds.top+bonds.bottom)/2, paint2);
+        paint2.getTextBounds(text5,0,text5.length(),bonds);
+        canvas.drawText(text5, 500,  middle-(bonds.top+bonds.bottom)/2, paint2);
+        paint2.getTextBounds(text6,0,text6.length(),bonds);
+        canvas.drawText(text6, 600,  middle-(bonds.top+bonds.bottom)/2, paint2);
     }
 }
